6991cc8fad67d4e7d663ba04e7a58bdb7f4fafd0
Gradoma <gradomski25@gmail.com>
2020-10-27 20:10:17 +0300

    tagCertificateDao added

---
+++ module #2. REST API Basics/src/main/java/com/epam/esm/dao/column/TagCertificateConst.java
@@ -0,0 +1,8 @@
+package com.epam.esm.dao.column;
+
+public class TagCertificateConst {
+    public static final String TABLE_TAG_CERT = "tag_certificate";
+    public static final String ID = "Id";
+    public static final String TAG_ID = "TagId";
+    public static final String CERTIFICATE_ID = "CertificateId";
+}
