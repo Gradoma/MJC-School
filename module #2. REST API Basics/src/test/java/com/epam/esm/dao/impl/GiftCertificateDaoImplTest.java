ed6fb05f616b63f77c68ec2f64a20d46bd2ac4a0
Gradoma <gradomski25@gmail.com>
2020-11-12 17:18:13 +0300

    some tests

--- module #2. REST API Basics/src/test/java/com/epam/esm/dao/impl/GiftCertificateDaoImplTest.java
+++ module #2. REST API Basics/src/test/java/com/epam/esm/dao/impl/GiftCertificateDaoImplTest.java
@@ -8,6 +8,8 @@ import com.epam.esm.dao.column.TagTableConst;
 import com.epam.esm.dao.column.TagToCertificateTableConst;
 import com.epam.esm.entity.GiftCertificate;
 import com.epam.esm.entity.Tag;
+import com.epam.esm.exception.DuplicateException;
+import com.epam.esm.exception.ResourceNotFoundException;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,14 +36,9 @@ import static org.junit.jupiter.api.Assertions.*;
 @ContextConfiguration(classes = {SpringTestConfig.class})
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SqlGroup({
-        @Sql("/db/certificate-data.sql"),
-        @Sql("/db/tag-data.sql")
+        @Sql("/db/certificate-data.sql")
 })
-class GiftCertificateDaoImplTest {      //todo (independent tests)
-    private static final String DESCRIPTION_KEYWORD_DESCRIPTION = "description";
-    private static final int DESCRIPTION_COUNT = 3;
-    private static final String NAME_KEYWORD_TEXT = "name";
-    private static final int TEXT_COUNT = 3;
+class GiftCertificateDaoImplTest {
     private static GiftCertificate certificate;
     @Autowired
     private JdbcTemplate jdbcTemplate;
@@ -75,27 +72,99 @@ class GiftCertificateDaoImplTest {      //todo (independent tests)
     }
 
     @Test
-    void add() {
-        int startRows = JdbcTestUtils.countRowsInTable(jdbcTemplate, GiftCertificateTableConst.TABLE_CERTIFICATE);
-        int startRowsInTagToCertTable = JdbcTestUtils.countRowsInTable(jdbcTemplate,
-                TagToCertificateTableConst.TABLE_TAG_CERT);
-        long generatedId = giftCertificateDao.add(certificate);
-        certificate.setId(generatedId);
-        assertEquals(startRows + 1,
-                JdbcTestUtils.countRowsInTable(jdbcTemplate, GiftCertificateTableConst.TABLE_CERTIFICATE));
-        assertEquals(startRowsInTagToCertTable + certificate.getTagList().size(),
-                JdbcTestUtils.countRowsInTable(jdbcTemplate, TagToCertificateTableConst.TABLE_TAG_CERT));
-    }
-
-    @Test
     void findById() {
-        long id = giftCertificateDao.add(certificate);
-        certificate.setId(id);
+        GiftCertificate cert = new GiftCertificate();
+        cert.setName("Name 2");
+        cert.setDescription("Desc 2");
+        cert.setPrice(35.15);
+        ZonedDateTime creationDate = ZonedDateTime.now().truncatedTo(ChronoUnit.MILLIS);
+        cert.setCreateDate(creationDate);
+        cert.setLastUpdateDate(cert.getCreateDate());
+        cert.setDuration(Duration.ofDays(20));
+
+        long id = giftCertificateDao.add(cert);
+        cert.setId(id);
         GiftCertificate fromDb = giftCertificateDao.findById(id);
         ZonedDateTime createDate = fromDb.getCreateDate();
         fromDb.setCreateDate(createDate.withZoneSameInstant(ZoneId.systemDefault()));
         ZonedDateTime lastUpdDate = fromDb.getLastUpdateDate();
         fromDb.setLastUpdateDate(lastUpdDate.withZoneSameInstant(ZoneId.systemDefault()));
-        assertEquals(certificate, fromDb);
+        assertEquals(cert, fromDb);
     }
+
+    @Test
+    void findById_negative(){
+        GiftCertificate cert = new GiftCertificate();
+        cert.setName("Name 2");
+        cert.setDescription("Desc 2");
+        cert.setPrice(35.15);
+        ZonedDateTime creationDate = ZonedDateTime.now().truncatedTo(ChronoUnit.MILLIS);
+        cert.setCreateDate(creationDate);
+        cert.setLastUpdateDate(cert.getCreateDate());
+        cert.setDuration(Duration.ofDays(20));
+        long id = giftCertificateDao.add(cert);
+
+        long incorrectId = id * 31;
+        assertThrows(ResourceNotFoundException.class, () -> giftCertificateDao.findById(incorrectId));
+    }
+
+    @Test
+    void findByCriteria(){
+
+        GiftCertificate certABC = new GiftCertificate();
+        certABC.setName("Cert B");
+        certABC.setDescription("Desc B");
+        certABC.setPrice(52.23);
+        ZonedDateTime creationDate = ZonedDateTime.now().truncatedTo(ChronoUnit.MILLIS);
+        certABC.setCreateDate(creationDate);
+        certABC.setLastUpdateDate(certABC.getCreateDate());
+        certABC.setDuration(Duration.ofDays(17));
+
+        GiftCertificate certBC = new GiftCertificate();
+        certBC.setName("Cert B C");
+        certBC.setDescription("Desc B C");
+        certBC.setPrice(52.23);
+        ZonedDateTime creationDateBC = ZonedDateTime.now().truncatedTo(ChronoUnit.MILLIS);
+        certBC.setCreateDate(creationDateBC);
+        certBC.setLastUpdateDate(certBC.getCreateDate());
+        certBC.setDuration(Duration.ofDays(17));
+
+        GiftCertificate certAC = new GiftCertificate();
+        certAC.setName("Cert A C");
+        certAC.setDescription("Desc A C");
+        certAC.setPrice(52.23);
+        ZonedDateTime creationDateAC = ZonedDateTime.now().truncatedTo(ChronoUnit.MILLIS);
+        certAC.setCreateDate(creationDateAC);
+        certAC.setLastUpdateDate(certAC.getCreateDate());
+        certAC.setDuration(Duration.ofDays(17));
+
+        giftCertificateDao.add(certABC);
+        giftCertificateDao.add(certBC);
+        giftCertificateDao.add(certAC);
+
+        List<GiftCertificate> certList2 = giftCertificateDao.findByCriteria(GiftCertificateDao.BY_NAME_AND_DESCRIPTION,
+                null, "a", "desc");
+        assertEquals(3, certList2.size());
+    }
+
+    @Test
+    void delete(){
+        GiftCertificate cert = new GiftCertificate();
+        cert.setName("Name 2");
+        cert.setDescription("Desc 2");
+        cert.setPrice(35.15);
+        ZonedDateTime creationDate = ZonedDateTime.now().truncatedTo(ChronoUnit.MILLIS);
+        cert.setCreateDate(creationDate);
+        cert.setLastUpdateDate(cert.getCreateDate());
+        cert.setDuration(Duration.ofDays(20));
+
+        long id = giftCertificateDao.add(cert);
+        int rowsAfterAdd = JdbcTestUtils.countRowsInTable(jdbcTemplate,
+                GiftCertificateTableConst.TABLE_CERTIFICATE);
+        giftCertificateDao.delete(id);
+        assertEquals(rowsAfterAdd - 1, JdbcTestUtils.countRowsInTable(jdbcTemplate,
+                GiftCertificateTableConst.TABLE_CERTIFICATE));
+    }
+
+
 }
